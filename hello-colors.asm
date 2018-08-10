f2eca717469c40d3899eeea31dd1c8dd431d2e85
jdferrell3 <jdferrell3>
2018-08-10 10:37:24 -0400

    remove colors from hello.asm, add hello-colors.asm

--- hello-colors.asm
+++ hello-colors.asm
@@ -35,17 +35,23 @@ main:
     ; clear screen
     jsr $e544
 
-    lda #$00
+    lda #$03
     sta BACKGROUND ; store A (03) in background color address
-    lda #$00
+    lda #$05
     sta BORDER     ; store A (05) in border color address
 
+    ldy #$00
     ldx #$00
 
 hello:
+    ; TODO change color for each char
+    sty $d800   ; text color
+    lda $d800,x
+
     lda text,x
     ; $0400 is first address of screen memory
     ; 40 col x 25 row = 1000 bytes
+    ; +5 starts 5 col from the left corner
     sta $0400,x
     inx
     cpx #11     ; length of text
