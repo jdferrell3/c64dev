9f18f4d22190f9a231dc41fdc521a6e57fd1dd00
jdferrell3 <jdferrell3>
2018-08-10 00:53:25 -0400

    whitespace

--- hello.asm
+++ hello.asm
@@ -32,37 +32,37 @@ BORDER=$d021
 jsr main
 
 main:
-	; clear screen
-	jsr $e544
+    ; clear screen
+    jsr $e544
 
-	lda #$03
-	sta BACKGROUND ; store A (03) in background color address
-	lda #$05
-	sta BORDER     ; store A (05) in border color address
+    lda #$03
+    sta BACKGROUND ; store A (03) in background color address
+    lda #$05
+    sta BORDER     ; store A (05) in border color address
 
     ldy #$00
-	ldx #$00
+    ldx #$00
 
 hello:
     ; TODO change color for each char
     sty $d800   ; text color
-	lda text,x
-	; $0400 is first address of screen memory
-	; 40 col x 25 row = 1000 bytes
-	; +5 starts 5 col from the left corner
-	sta $0400,x
-	; iny
-	inx
-	cpx #11     ; length of text
-	bne hello   ; loop until x == 11
-	jsr wait
-	jsr end
+    lda text,x
+    ; $0400 is first address of screen memory
+    ; 40 col x 25 row = 1000 bytes
+    ; +5 starts 5 col from the left corner
+    sta $0400,x
+    ; iny
+    inx
+    cpx #11     ; length of text
+    bne hello   ; loop until x == 11
+    jsr wait
+    jsr end
 
 ; infinite loop so text remains on screen
 wait:
-	jsr wait
+    jsr wait
 
 end:
 
 text:
-	!scr "hello world"
+    !scr "hello world"
